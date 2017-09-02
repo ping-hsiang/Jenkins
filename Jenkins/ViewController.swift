be640deb52a5d00d3d8a585c6e4a0ea185c3fafe
HSU PING HSIANG <hsu@HSUde-MacBook-Pro.local>
2017-09-02 14:23:05 +0800

    #2

---
+++ Jenkins/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Jenkins
+//
+//  Created by HSU PING HSIANG on 2017/9/2.
+//  Copyright © 2017年 許秉翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    //test
+    
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
